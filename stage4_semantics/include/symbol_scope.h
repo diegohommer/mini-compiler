3baf57ce85fb42920ce95d3edde6af7a6260d055
diegohommer <daugsteel@gmail.com>
2025-05-13 21:55:31 -0300

    implementing scope (symbol table stack) module

---
+++ stage4_semantics/include/symbol_scope.h
@@ -0,0 +1,31 @@
+#ifndef SCOPE_SCOPE_H
+#define SCOPE_SCOPE_H
+
+#include "table.h"
+
+typedef struct {
+    symbol_table_t** tables;
+    int num_tables;
+} scope_stack_t;
+
+/*
+ * Function scope_stack_new, creates an empty symbol table stack.
+ */
+scope_stack_t* scope_stack_new(void);
+
+/*
+ * Function scope_stack_free, recursively frees the given stack and its tables.
+ */
+void scope_stack_free(scope_stack_t* stack);
+
+/*
+ * Function scope_push, creates a new scope (symbol table) and appends it to the given stack.
+ */
+void scope_push(scope_stack_t* stack);
+
+/*
+ * Function scope_pop, removes the scope that's on top of the stack and free its contents.
+ */
+void scope_pop(scope_stack_t* stack);
+
+#endif  // SCOPE_SCOPE_H
---
+++ stage4_semantics/src/semantics/symbol_scope.c
@@ -0,0 +1,45 @@
+#include "symbol_scope.h"
+
+scope_stack_t* scope_stack_new(void)
+{
+    scope_stack_t* new_stack = NULL;
+    new_stack = calloc(1, sizeof(scope_stack_t));
+    if (new_stack != NULL) {
+        new_stack->num_tables = 0;
+        new_stack->tables = NULL;
+    }
+    return new_stack;
+}
+
+void scope_stack_free(scope_stack_t* stack)
+{
+    int i;
+    for (i = 0; i < stack->num_tables; i++) {
+        table_free(stack->tables[i]);
+    }
+    free(stack->tables);
+    free(stack);
+}
+
+void scope_push(scope_stack_t* stack)
+{
+    if (stack != NULL) {
+        symbol_table_t* new_scope = table_new();
+        stack->num_tables++;
+        stack->tables = realloc(stack->tables, stack->num_tables * sizeof(symbol_table_t*));
+        stack->tables[stack->num_tables - 1] = new_scope;
+    } else {
+        printf("Error: %s received NULL scope stack = %p.\n", __FUNCTION__, stack);
+    }
+}
+
+void scope_pop(scope_stack_t* stack)
+{
+    if (stack != NULL) {
+        table_free(stack->tables[stack->num_tables - 1]);
+        stack->num_tables--;
+        stack->tables = realloc(stack->tables, stack->num_tables * sizeof(symbol_table_t*));
+    } else {
+        printf("Error: %s received NULL scope stack = %p.\n", __FUNCTION__, stack);
+    }
+}
