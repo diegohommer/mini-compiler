5d762842fdab2836e3c17287dd9c0fd80619a463
diegohommer <daugsteel@gmail.com>
2025-06-15 14:39:43 -0300

    feat(main): print iloc code to stdout

--- stage5_codegen/src/main.c
+++ stage5_codegen/src/main.c
@@ -15,6 +15,7 @@ int main(int argc, char **argv)
     scope_stack = scope_stack_new();
     int ret = yyparse();
     if (tree) {
+        print_iloc_list(tree->code);
         iloc_op_list_free(tree->code);
         asd_free(tree);
     }
