e431a3670cc67b9b67447dae5a66cfc68ace712c
diegohommer <daugsteel@gmail.com>
2025-07-07 20:51:36 -0300

    feat(asm_gen): create assembler generator module files

---
+++ stage6_assembly/include/asm_gen.h
@@ -0,0 +1,11 @@
+/*
+ * Developed by: Diego Hommerding Amorim - 00341793
+ *           Gabriel Kenji Yatsuda Ikuta - 00337491
+ */
+
+#ifndef ASM_GEN_H
+#define ASM_GEN_H
+
+
+#endif  // ASM_GEN_H
+
---
+++ stage6_assembly/src/codegen/asm_gen.c
@@ -0,0 +1,5 @@
+/*
+ * Developed by: Diego Hommerding Amorim - 00341793
+ *           Gabriel Kenji Yatsuda Ikuta - 00337491
+ */
+#include "asm_gen.h"
