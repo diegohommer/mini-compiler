4a41f9bafe21350d9d0e5ace410d08637248a9c3
diegohommer <daugsteel@gmail.com>
2025-07-08 01:09:06 -0300

    refactor(main): print asm instead of iloc Change so main outputs assembly code instead of iloc.

--- stage6_assembly/src/main.c
+++ stage6_assembly/src/main.c
@@ -3,6 +3,7 @@
  *           Gabriel Kenji Yatsuda Ikuta - 00337491
  */
 
+#include "asm_gen.h"
 #include "iloc_ir.h"
 #include "scope_stack.h"
 extern int yyparse(void);
@@ -15,7 +16,7 @@ int main(int argc, char **argv)
     scope_stack = scope_stack_new();
     int ret = yyparse();
     if (tree) {
-        print_iloc_list(tree->code);
+        print_asm(scope_stack->tables[0], tree->code);
         iloc_op_list_free(tree->code);
         asd_free(tree);
     }
